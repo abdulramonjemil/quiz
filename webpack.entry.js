23fd50b35528036970040eae32e291ff8d4b16c7
Abdulramon Jemil <80219104+abdulramonjemil@users.noreply.github.com>
2023-02-04 15:25:45 +0100

    Modify explanation comment in webpack entry

--- webpack.entry.js
+++ webpack.entry.js
@@ -1,7 +1,7 @@
 /**
  * This file is used as entry point for webpack. Quizzes can then be created
- * by calling 'createQuiz' after linking the generated javascript file
- * with an HTML page causing the function to be defined in the global scope.
+ * by calling 'Quiz.create' after linking the generated javascript file
+ * with an HTML page (causing the object to be defined in the global scope).
  */
 
 import Quiz from "./index"
