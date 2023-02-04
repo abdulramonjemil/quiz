6e2feac14f7d84902cc0a011229bd362653ae520
Abdulramon Jemil <80219104+abdulramonjemil@users.noreply.github.com>
2023-02-04 15:59:07 +0100

    Modify polyfiller

--- core/polyfiller.js
+++ core/polyfiller.js
@@ -3,13 +3,16 @@
  * The polyfill used is at "https://github.com/GoogleChromeLabs/container-query-polyfill"
  */
 function polyfillContainerQueries() {
-  const CQ_POLYFILL_URL =
-    "https://cdn.jsdelivr.net/npm/container-query-polyfill@1/dist/container-query-polyfill.modern.js"
-
   const containerQueriesAreSupported =
     "container" in document.documentElement.style
 
-  if (!containerQueriesAreSupported) import(CQ_POLYFILL_URL)
+  if (!containerQueriesAreSupported) {
+    const CQ_POLYFILL_URL =
+      "https://cdn.jsdelivr.net/npm/container-query-polyfill@1/dist/container-query-polyfill.modern.js"
+    const script = document.createElement("script")
+    script.src = CQ_POLYFILL_URL
+    document.head.appendChild(script)
+  }
 }
 
 function polyfillForBrowser() {
