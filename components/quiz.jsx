cc5cd4b93f386c2b44132c25b65cf4892b9cfbee
Abdulramon Jemil <80219104+abdulramonjemil@users.noreply.github.com>
2023-02-25 12:46:39 +0100

    Change quiz root to use the section element

--- components/quiz.jsx
+++ components/quiz.jsx
@@ -9,11 +9,6 @@ export default class Quiz extends Component {
   /** @protected */
   /* eslint-disable-next-line class-methods-use-this */
   $render() {
-    return (
-      <div className={styles.quiz}>
-        I am <code>Quizy</code>
-        <br /> Okay?
-      </div>
-    )
+    return <section className={styles.quiz}>A section content</section>
   }
 }
