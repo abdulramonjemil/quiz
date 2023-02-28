8d7bd204dfa46a2607175b118e56dc12273645f4
Abdulramon Jemil <80219104+abdulramonjemil@users.noreply.github.com>
2023-02-28 12:35:46 +0100

    Rename parameter passed to Header component

--- components/header.jsx
+++ components/header.jsx
@@ -1,11 +1,11 @@
 import { phraseToNode } from "../core/content-parser"
 import styles from "../scss/header.module.scss"
 
-export default function Header({ labellingId }, children) {
+export default function Header({ labellingId }, content) {
   return (
     <header>
       <h1 className={styles.header} id={labellingId}>
-        {phraseToNode(children)}
+        {phraseToNode(content)}
       </h1>
     </header>
   )
