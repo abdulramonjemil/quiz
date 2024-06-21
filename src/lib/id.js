6eb7761ddd4e07dc632c1f1ecefde308a6b8aa96
Abdulramon Jemil <141630049+abdulramon-jemil@users.noreply.github.com>
2024-06-21 03:27:31 +0100

    Change structure of unique id generated

--- src/lib/id.js
+++ src/lib/id.js
@@ -1,6 +1,6 @@
 export function uniqueId() {
   const timeStamp = String(performance.now()).replace(".", "-")
   const randomNumberWithoutDot = String(Math.random()).slice(2)
-  const finalUniqueId = `uuid__${timeStamp}-${randomNumberWithoutDot}`
+  const finalUniqueId = `:id:-${timeStamp}:-${randomNumberWithoutDot}`
   return finalUniqueId
 }
