a18615bca84d4f9b18694a4852cca1f6b81fc0bd
Abdulramon Jemil <141630049+abdulramon-jemil@users.noreply.github.com>
2024-07-28 03:18:35 +0100

    Load prismcss from js to allow config

--- src/components/code-board.jsx
+++ src/components/code-board.jsx
@@ -14,8 +14,24 @@ import Styles from "@/scss/code-board.module.scss"
  * }} CodeBoardProps
  */
 
+const DEFAULT_PRISM_THEME_URL =
+  "https://cdn.jsdelivr.net/gh/PrismJS/prism-themes/themes/prism-material-oceanic.css"
 const PRISMJS_COMPONENTS_CDN_URL =
   "https://cdn.jsdelivr.net/npm/prismjs@1.x/components/"
+
+/** @param {string} url  */
+export function loadPrismTheme(url) {
+  const linkToInsert = /** @type {HTMLLinkElement} */ (
+    <link rel="stylesheet" href={url} />
+  )
+  const links = Array.from(document.head.querySelectorAll("link"))
+  const sameLinkExists = links.some(
+    (link) => link.rel === "stylesheet" && link.href === linkToInsert.href
+  )
+
+  if (sameLinkExists) return
+  document.head.prepend(linkToInsert)
+}
 
 window.Prism = window.Prism || {}
 Prism.plugins.autoloader.languages_path = PRISMJS_COMPONENTS_CDN_URL
@@ -23,7 +39,9 @@ Prism.manual = true
 
 /** @param {CodeBoardProps} param0 */
 export default function CodeBoard({ snippet, language, title }) {
+  loadPrismTheme(DEFAULT_PRISM_THEME_URL)
   const codeRefHolder = createElementRefHolder()
+
   const codeBoardNode = (
     <div className={Styles.CodeBoard}>
       <p className={Styles.CodeBoard__Title}>{phraseToNode(title)}</p>
